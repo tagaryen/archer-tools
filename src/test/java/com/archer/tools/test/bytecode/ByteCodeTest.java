e0bd787af9c3d54b990a59aca3b991683842fb8e
xuyi <xu_yi@cestc.cn>
2026-05-12 15:23:59 +0800

    update tests

--- src/test/java/com/archer/tools/test/bytecode/ByteCodeTest.java
+++ src/test/java/com/archer/tools/test/bytecode/ByteCodeTest.java
@@ -22,63 +22,63 @@ public class ByteCodeTest {
 	public static void test() {
 
 		try {
-			
-			/***
-			 * here we generate a new class
-			 * public class ClassA$Impl extends ClassA {
-			 *     public String nameCp;
-			 *     public void setName(String name, int age) {
-			 *         this.nameCp = name;
-			 *         super.setName(null, age);
-			 *     }
-			 * }
-			 * */
-			ClassBytecode impl = (new ClassBytecode()).readAndDecodeClass(ClassA.class).generateImplClass("ClassA$Impl", "com.archer.tools.test.bytecode");
-			
-			/**
-			 * Add constant pool
-			 * 1. add field com.archer.tools.test.bytecode.ClassA$Impl.nameCp Ljava/lang/String
-			 * 2. add invoke method com/archer/tools/test/bytecode/ClassA.setName()
-			 * */
-			impl.addField("nameCp", String.class, 1);
-			int fieldIndex = impl.getConstantPool().findField("nameCp", String.class);
-			int methodIndex = impl.getConstantPool().addMethod("setName", new String[] {"Ljava/lang/String;","I"}, "V", "com/archer/tools/test/bytecode/ClassA");
-			
-			
-			/**
-			 * Add override method to com.archer.tools.test.bytecode.ClassA$Impl:
-			 * public void setName(String name, int age) {
-			 *     this.nameCp = name;
-			 *     super.setName(null, age);
-			 * }
-			 * */
-			CodeAttributeWriter writer = CodeAttributeWriter.of(impl.getConstantPool().findName("Code"));
-			writer.addInstruction("aload_0")
-				.addInstruction("aload_1")
-				.addInstruction16("putfield", fieldIndex)
-				.addInstruction("aload_0")
-				.addInstruction("aconst_null")
-				.addInstruction("iload_2")
-				.addInstruction16("invokespecial", methodIndex)
-				.addInstruction("return");
-			impl.addMethod("setName", new String[] {"Ljava/lang/String;","I"}, "V", writer.toCodeAttribute());
-			
-			
-			/**
-			 * New com.archer.tools.test.bytecode.ClassA$Impl()
-			 * */
-			ClassA classAins = (ClassA)ClassUtil.newInstance(impl.loadSelfClass());
-			classAins.setName("xuyihaoshuai", 18);
+		
+		/***
+		 * here we generate a new class
+		 * public class ClassA$Impl extends ClassA {
+		 *     public String nameCp;
+		 *     public void setName(String name, int age) {
+		 *         this.nameCp = name;
+		 *         super.setName(null, age);
+		 *     }
+		 * }
+		 * */
+		ClassBytecode impl = (new ClassBytecode()).readAndDecodeClass(ClassA.class).generateImplClass("ClassA$Impl", "com.archer.tools.test.bytecode");
+		
+		/**
+		 * Add constant pool
+		 * 1. add field com.archer.tools.test.bytecode.ClassA$Impl.nameCp Ljava/lang/String
+		 * 2. add invoke method com/archer/tools/test/bytecode/ClassA.setName()
+		 * */
+		impl.addField("nameCp", String.class, 1);
+		int fieldIndex = impl.getConstantPool().findField("nameCp", String.class);
+		int methodIndex = impl.getConstantPool().addMethod("setName", new String[] {"Ljava/lang/String;","I"}, "V", "com/archer/tools/test/bytecode/ClassA");
+		
+		
+		/**
+		 * Add override method to com.archer.tools.test.bytecode.ClassA$Impl:
+		 * public void setName(String name, int age) {
+		 *     this.nameCp = name;
+		 *     super.setName(null, age);
+		 * }
+		 * */
+		CodeAttributeWriter writer = CodeAttributeWriter.of(impl.getConstantPool().findName("Code"));
+		writer.addInstruction("aload_0")
+			.addInstruction("aload_1")
+			.addInstruction16("putfield", fieldIndex)
+			.addInstruction("aload_0")
+			.addInstruction("aconst_null")
+			.addInstruction("iload_2")
+			.addInstruction16("invokespecial", methodIndex)
+			.addInstruction("return");
+		impl.addMethod("setName", new String[] {"Ljava/lang/String;","I"}, "V", writer.toCodeAttribute());
+		
+		
+		/**
+		 * New com.archer.tools.test.bytecode.ClassA$Impl()
+		 * */
+		ClassA classAins = (ClassA)ClassUtil.newInstance(impl.loadSelfClass());
+		classAins.setName("xuyihaoshuai", 18);
 
-			/**
-			 * Invoke methods for tests
-			 * */
-			System.out.println(classAins.getName());
-			System.out.println(classAins.getAge());
-			
-			Field f = classAins.getClass().getDeclaredField("nameCp");
-			String nameCp = (String) f.get(classAins);
-			System.out.println(nameCp);
+		/**
+		 * Invoke methods for tests
+		 * */
+		System.out.println(classAins.getName());
+		System.out.println(classAins.getAge());
+		
+		Field f = classAins.getClass().getDeclaredField("nameCp");
+		String nameCp = (String) f.get(classAins);
+		System.out.println(nameCp);
 
 			System.out.println(classAins.getClass().getName());
 		} catch (Exception e) {
